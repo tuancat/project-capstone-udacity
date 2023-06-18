ab824639e1e03aa729b05a49b1fb47fcf3e8aa61
Tuan Cat <tuan.catnghiemhieu@gmail.com>
2023-06-18 14:04:43 +0700

    - update

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def home():
 
 if __name__ == "__main__":
     # load pretrained model as clf
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=5000, debug=True) # specify port=80
