52ff60714f7de947a36d53c3d396bbf40ecff797
Tuan Cat <tuan.catnghiemhieu@gmail.com>
2023-06-18 11:18:51 +0700

    - update

---
+++ scripts/upload-docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="tuancnh/ml-classification"
+
+
+# Step 2:
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+# docker login --username phamanhtuan221995
+docker image tag ml-classification $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
